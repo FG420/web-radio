825cfaa22cbe461de66b944da42966edba54529d
FG420 <facchinatog97@gmail.com>
2024-09-11 18:31:55 +0200

    some stuff ok...

--- main.go
+++ main.go
@@ -1,28 +1,57 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/FG420/web-radio/radio"
 )
 
 // type Data struct {
-// 	Station *radio.Station
+// 	Stations []*Station
 // }
 
-func handleHome(w http.ResponseWriter, req *http.Request) {
-	templ := template.Must(template.ParseFiles("index.html"))
+type Station struct {
+	ID      string
+	Name    string
+	Url     string
+	Country string
+	Tags    string
+	Favicon string
+}
 
-	templ.Execute(w, nil)
+func NewStation(id, name, url, country, tags, favicon string) *Station {
+	return &Station{
+		ID:      id,
+		Name:    name,
+		Url:     url,
+		Country: country,
+		Tags:    tags,
+		Favicon: favicon,
+	}
+}
+
+func handleHome(w http.ResponseWriter, req *http.Request) {
+	// templ := template.Must(template.ParseFiles("views/index.html"))
+	// templ.Execute(w, nil)
+
+	templ := template.Must(template.ParseFiles("views/data.html"))
+	getStations := radio.GetStationsByCountry("italy")
+
+	var s *Station
+	for i := 0; i < len(getStations); i++ {
+		ss := NewStation(getStations[i].StationUUID, getStations[i].Name, getStations[i].URL,
+			getStations[i].Country, getStations[i].Tags, getStations[i].Favicon)
+		s = ss
+	}
+
+	log.Println(s)
+
+	templ.ExecuteTemplate(w, "stationData", s)
 }
 
 func main() {
-
-	stations := radio.GetStationsByCountry("italy")
-
-	log.Println(stations)
 
 	http.HandleFunc("/", handleHome)
 
--- radio/radio.go
+++ radio/radio.go
@@ -4,36 +4,6 @@ import (
 	"gitlab.com/AgentNemo/goradios"
 )
 
-type Station struct {
-	ID      string
-	Name    string
-	Url     string
-	Country string
-	Tags    string
-	Favicon string
-}
-
-func NewStation(id, name, url, country, tags, favicon string) *Station {
-	return &Station{
-		ID:      id,
-		Name:    name,
-		Url:     url,
-		Country: country,
-		Tags:    tags,
-		Favicon: favicon,
-	}
-}
-
-func GetStationsByCountry(country string) []*Station {
-	getStations := goradios.FetchStations(goradios.StationsByCountry, country)
-
-	var stations []*Station
-
-	for i := 0; i < len(getStations); i++ {
-		station := NewStation(getStations[i].StationUUID, getStations[i].Name, getStations[i].URL,
-			getStations[i].Country, getStations[i].Tags, getStations[i].Favicon)
-		stations = append(stations, station)
-	}
-
-	return stations
+func GetStationsByCountry(country string) []goradios.Station {
+	return goradios.FetchStations(goradios.StationsByCountry, country)
 }
