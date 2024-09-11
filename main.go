541d575142f1e154360f69e9a581ee0251486c40
FG420 <facchinatog97@gmail.com>
2024-09-11 16:10:39 +0200

    ..

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"html/template"
 	"log"
+	"net/http"
 
 	"github.com/FG420/web-radio/radio"
 )
@@ -10,9 +12,20 @@ import (
 // 	Station *radio.Station
 // }
 
+func handleHome(w http.ResponseWriter, req *http.Request) {
+	templ := template.Must(template.ParseFiles("index.html"))
+
+	templ.Execute(w, nil)
+}
+
 func main() {
 
 	stations := radio.GetStationsByCountry("italy")
 
 	log.Println(stations)
+
+	http.HandleFunc("/", handleHome)
+
+	http.ListenAndServe(":8080", nil)
+
 }
